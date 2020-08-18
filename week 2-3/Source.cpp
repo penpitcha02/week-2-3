d3e6810feea3a4c5a5317b80137a7991632d9fdf
PENPITCHA SAPPAKUN <63010709@KMITL.AC.TH>
2020-08-18 19:28:54 +0700

    ดาวเสร็จ

--- week 2-3/Source.cpp
+++ week 2-3/Source.cpp
@@ -1,16 +1,38 @@
 #include<stdio.h>
 int main()
 {
-	int a = 1, b = 1, m;
-	printf("Enter number : ");
-	scanf_s("%d", &m);
-    while (a <= m)
+    int n;
+    printf("Enter n: ");
+    scanf_s("%d", &n);
+    int i = 1, k = 1, l = 1;
+    while (k <= n)
     {
-        if (a == 1 || a == m )
-            printf("*");
-        else
-            printf(" ");
+        printf("*");
+        k++;
     }
-
+    printf("\n");
+    while (i <= n - 2)
+    {
+        int j = 1;
+        while (j <= n)
+        {
+            if (j == 1 || j == n)
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
+            j++;
+        }
+        printf("\n");
+        i++;
+    }
+    while (l <= n)
+    {
+        printf("*");
+        l++;
+    }
     return 0;
 }
